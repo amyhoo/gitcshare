d06d0152d60e5affd895e6cb7fc94fb27a8fb2b5
徐亚敏 <yamin_xu@163.com>
2016-03-21 10:08:16 +0800

    trim

--- local/local_settings.py
+++ local/local_settings.py
@@ -8,17 +8,17 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "bond",
-        'USER': 'richard',  #mysql用户名，留空则默认为当前linux用户名
-        'PASSWORD': 'xumin06',   #mysql密码
-        'HOST': '192.168.1.103',  #留空默认为localhost
+        'NAME': "",
+        'USER': '',  #mysql用户名，留空则默认为当前linux用户名
+        'PASSWORD': '',   #mysql密码
+        'HOST': '',  #留空默认为localhost
         'PORT': '5432',  #留空默认为3306端口
         #'CONN_MAX_AGE':3600
     }
 }
 SQLALCHEMY_DATABASE={
     "type":"postgres",
-    "connection_uri":"postgres://richard:xumin06@192.168.1.103:5432/bond",#数据库连接字符串
+    "connection_uri":"postgres://",#数据库连接字符串
     "encoding":"utf-8",#数据库编码字符串
     "pool_size":5, #连接线程池
     "max_retry":10, #最大重试次数
